9df4bc88acd6cc641fc669ae60c1d645291949eb
Ashrend <ashrend@users.noreply.github.com>
2026-05-31 10:21:39 +0800

    chore: bump version to 1.2.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "aff.importer.tool"
         minSdk = 26
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.2.0"
+        versionCode = 3
+        versionName = "1.2.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
