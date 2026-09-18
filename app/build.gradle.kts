43b3026434d10dde319ada0e5e48d6e5fa108d29
unknown <ashrendv@outlook.com>
2026-09-18 09:50:38 +0800

    chore: bump version to 1.2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "aff.importer.tool"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2.2"
+        versionCode = 4
+        versionName = "1.2.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
